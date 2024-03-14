693965165f8c2810701b606ccdfdb303cc3b4d53
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2024-03-14 23:37:56 +0300

    header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,23 +5,28 @@ import style from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={style["header"]}>
-      <Container>
-        <div className={style["header__wrapper"]}>
-          <Logo />
+      <div className={style["header__top"]}>
+        <Container>sdasd</Container>
+      </div>
+      <div className={style["header__main"]}>
+        <Container>
+          <div className={style["header__wrapper"]}>
+            <Logo />
 
-          <div className={style["header__right-side"]}>
-            <button className={style["header__login-btn"]}>
-              <img src="/icons/login.svg" alt="" />
-            </button>
-            <button className={style["header__search-btn"]}>
-              <img src="/icons/search.svg" alt="" />
-            </button>
-            <button className={style["header__categories-btn"]}>
-              <span className={style["header__categories-icon"]} />
-            </button>
+            <div className={style["header__right-side"]}>
+              <button className={style["header__login-btn"]}>
+                <img src="/icons/login.svg" alt="" />
+              </button>
+              <button className={style["header__search-btn"]}>
+                <img src="/icons/search.svg" alt="" />
+              </button>
+              <button className={style["header__categories-btn"]}>
+                <span className={style["header__categories-icon"]} />
+              </button>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </header>
   );
 };
